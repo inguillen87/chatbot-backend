de23c137a808b6e28c0c70f3f04d8cc7f50e8207
inguillen87 <guillen.marce@gmail.com>
2025-06-13 00:49:54 -0300

    Update municipios.py

--- services/municipios.py
+++ services/municipios.py
@@ -432,6 +432,7 @@ class EngancheAnonimoMunicipioHandler(BaseMunicipioHandler):
         return None
 
 def responder_municipio(pregunta, user_obj, rubro_obj, **kwargs):
+    CONTEXTO_MUNICIPIO = "contexto_municipio"
     contexto_previo = kwargs.get('contexto_previo', {})
     contexto_municipio = contexto_previo.get(CONTEXTO_MUNICIPIO, {})
     context = {
@@ -461,7 +462,7 @@ def responder_municipio(pregunta, user_obj, rubro_obj, **kwargs):
         mensaje_transicion = "Entendido, cambiemos de tema. Sobre tu nueva consulta:\n\n"
         respuesta_final['respuesta'] = mensaje_transicion + respuesta_final['respuesta']
 
-    if "[nombre_vecino]" in respuesta_final.get('respuesta', ''):
+      if "[nombre_vecino]" in respuesta_final.get('respuesta', ''):
         nombre_vecino_memoria = contexto_municipio.get('nombre_vecino', 'vecino')
         respuesta_final['respuesta'] = respuesta_final['respuesta'].replace("[nombre_vecino]", nombre_vecino_memoria)
 
