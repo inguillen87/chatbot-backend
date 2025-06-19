361bde84de433b584df2e0578a5855aedde99afa
inguillen87 <guillen.marce@gmail.com>
2025-06-19 10:11:45 -0300

    sdf

---
+++ migrations/versions/7be0011e1eca_uno_ramas_de_migraciones_segunda_vez.py
@@ -0,0 +1,24 @@
+"""Uno ramas de migraciones (segunda vez)
+
+Revision ID: 7be0011e1eca
+Revises: 
+Create Date: 2025-06-19 10:09:48.729125
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7be0011e1eca'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/a225bb2cdf6c_unifico_ramas_de_migración.py
@@ -0,0 +1,24 @@
+"""Unifico ramas de migración
+
+Revision ID: a225bb2cdf6c
+Revises: 
+Create Date: 2025-06-19 10:07:54.971099
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a225bb2cdf6c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
