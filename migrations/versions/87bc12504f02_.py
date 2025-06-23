585ed18366bf1bf5182dd1155afda13d50373412
inguillen87 <guillen.marce@gmail.com>
2025-06-22 21:28:59 -0300

    sdf

---
+++ migrations/versions/87bc12504f02_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 87bc12504f02
+Revises: 2222abcd1234, 9915bd712f34
+Create Date: 2025-06-22 21:28:37.904078
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '87bc12504f02'
+down_revision = ('2222abcd1234', '9915bd712f34')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
