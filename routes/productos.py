0b8d1694a3324f5432d583bba704c9bc97eebf12
inguillen87 <guillen.marce@gmail.com>
2025-06-27 21:31:40 -0300

    Update productos.py

--- routes/productos.py
+++ routes/productos.py
@@ -4,7 +4,7 @@ from routes.catalogo import listar_catalogo
 
 productos_bp = Blueprint('productos_bp', __name__, url_prefix='/productos')
 
-@productos_bp.route('', methods=['GET'])
+@productos_bp.route('/', methods=['GET'], strict_slashes=False)
 @token_requerido
 def obtener_productos(user):
     # Reutiliza la lógica de listar_catalogo para exponer un alias en /productos
