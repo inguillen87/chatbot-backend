c8f7416f3309ff912bce84b1bbdbbb12164fb45a
inguillen87 <guillen.marce@gmail.com>
2025-07-15 18:45:29 -0300

    asd

---
+++ migrations/versions/8959ca5450ec_unifico_heads_de_migraciones.py
@@ -0,0 +1,24 @@
+"""Unifico heads de migraciones
+
+Revision ID: 8959ca5450ec
+Revises: 4b9953f012b9, a77eb3e5e243
+Create Date: 2025-07-15 16:39:15.163518
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8959ca5450ec'
+down_revision = ('4b9953f012b9', 'a77eb3e5e243')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
