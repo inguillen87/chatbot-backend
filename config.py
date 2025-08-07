101d28aad7d94233e31b401d6d68b93150d9b5be
inguillen87 <guillen.marce@gmail.com>
2025-08-07 20:00:35 -0300

    asd

--- config.py
+++ config.py
@@ -61,8 +61,8 @@ class Config:
 
     SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
     SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
-    SMTP_USER = os.getenv("SMTP_USER", "tucorreo@gmail.com")
-    SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", "tucontraseña")
+    SMTP_USER = os.getenv("SMTP_USER", "guillen.marce@gmail.com")
+    SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", "hwgkycwywmphzbot")
     SMTP_USE_TLS = os.getenv("SMTP_USE_TLS", "True").lower() in ('true', '1', 't')
     SMTP_USE_SSL = os.getenv("SMTP_USE_SSL", "False").lower() in ('true', '1', 't')
 
