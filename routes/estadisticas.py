df1c809f9491961bfe88193faf27c3f8de1cc454
inguillen87 <guillen.marce@gmail.com>
2025-08-26 18:31:46 -0300

    chore(heatmap): remove duplicate api

--- routes/estadisticas.py
+++ routes/estadisticas.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, current_app
+import os
 from sqlalchemy import text
 from utils.auth_helpers import token_requerido, admin_o_empleado_requerido
 from models import db, User
@@ -12,7 +13,8 @@ estadisticas_bp = Blueprint('estadisticas', __name__, url_prefix='/estadisticas'
 @admin_o_empleado_requerido
 def mapa_calor(current_user):
     """Renderiza el mapa de calor."""
-    return render_template('estadisticas.html')
+    maptiler_key = current_app.config.get("MAPTILER_KEY") or os.getenv("VITE_MAPTILER_KEY", "")
+    return render_template('estadisticas.html', maptiler_key=maptiler_key)
 
 @estadisticas_bp.route('/locations', methods=['GET'])
 @token_requerido
@@ -24,5 +26,15 @@ def get_user_locations(current_user):
     else:
         users = User.query.filter(User.empresa_id == current_user.id, User.latitud.isnot(None), User.longitud.isnot(None)).all()
 
-    locations = [{"lat": user.latitud, "lng": user.longitud} for user in users]
-    return jsonify(locations)
+    features = [
+        {
+            "type": "Feature",
+            "properties": {"weight": 1},
+            "geometry": {
+                "type": "Point",
+                "coordinates": [user.longitud, user.latitud],
+            },
+        }
+        for user in users
+    ]
+    return jsonify({"type": "FeatureCollection", "features": features})
