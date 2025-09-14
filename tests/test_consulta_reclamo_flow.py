19bb932e61cc926d54d7efa946d6af250e2e974d
inguillen87 <guillen.marce@gmail.com>
2025-09-14 00:24:06 -0300

    feat: handle ticket number directly

--- tests/test_consulta_reclamo_flow.py
+++ tests/test_consulta_reclamo_flow.py
@@ -99,6 +99,38 @@ class TestConsultaReclamoFlow(unittest.TestCase):
             ConversationState.ESPERANDO_NUMERO_TICKET.name
         )
 
+    def test_consulta_estado_reclamo_numero_inicial(self):
+        owner_user = User.query.get(1)
+        rubro_obj = owner_user.rubro
+        chat_context = ChatSessionContext(chat_session_id='test_consulta_numero_inicial', user_id=1, context_data={})
+        db.session.add(chat_context)
+        db.session.commit()
+
+        response = responder_municipio(
+            pregunta_original='123456',
+            owner_user=owner_user,
+            rubro_obj=rubro_obj,
+            viewer_user=owner_user,
+            chat_db_context=chat_context
+        )
+
+        self.assertIn("PIN", response["message_body"])
+        self.assertEqual(
+            chat_context.context_data['contexto_municipio_v2']['estado_conversacion'],
+            ConversationState.ESPERANDO_NUMERO_TICKET.name
+        )
+
+        response2 = responder_municipio(
+            pregunta_original='654321',
+            owner_user=owner_user,
+            rubro_obj=rubro_obj,
+            viewer_user=owner_user,
+            chat_db_context=chat_context
+        )
+
+        self.assertIn("en_proceso", response2["message_body"])
+        self.assertIsNone(chat_context.context_data['contexto_municipio_v2'].get('estado_conversacion'))
+
     def test_consulta_estado_reclamo_natural_language(self):
         owner_user = User.query.get(1)
         rubro_obj = owner_user.rubro
