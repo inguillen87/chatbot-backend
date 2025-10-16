a7aa2ca313333276744dc57bce737278e1f17cc8
inguillen87 <guillen.marce@gmail.com>
2025-10-16 13:23:38 -0300

    Update notifications.py

--- routes/notifications.py
+++ routes/notifications.py
@@ -1,27 +1,31 @@
 from flask import Blueprint, jsonify, make_response, request
 from models import User
 from routes.auth import token_requerido
+from utils.auth_helpers import _set_anon_cookie, get_or_create_anon_id
 
 notifications_bp = Blueprint('notifications', __name__)
 
 
 @notifications_bp.route('/notifications', methods=['OPTIONS'])
 def notifications_options():
-    """Preflight CORS handler for /notifications."""
-    response = make_response("", 200)
+    """Responder a los preflight requests con los encabezados necesarios."""
+    anon_id = get_or_create_anon_id()
+    resp = make_response("", 200)
     origin = request.headers.get("Origin")
     if origin:
-        response.headers["Access-Control-Allow-Origin"] = origin
-        response.headers["Vary"] = "Origin"
+        resp.headers["Access-Control-Allow-Origin"] = origin
+        resp.headers["Vary"] = "Origin"
     else:
-        response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = (
+        resp.headers["Access-Control-Allow-Origin"] = "*"
+    resp.headers["Access-Control-Allow-Headers"] = (
         "Authorization, Content-Type, Origin, Accept, "
         "X-Entity-Token, X-Chat-Session-Id, X-Anon-Id, Anon-Id"
     )
-    response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
+    resp.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    resp.headers["Access-Control-Allow-Credentials"] = "true"
+    resp.headers.setdefault("X-Anon-Id", anon_id)
+    resp.headers.setdefault("Anon-Id", anon_id)
+    return _set_anon_cookie(resp, anon_id)
 
 
 @notifications_bp.route('/notifications', methods=['GET'])
