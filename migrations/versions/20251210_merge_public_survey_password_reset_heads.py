b53e1420157776ac86e04be93e6ea90215cdc0a7
inguillen87 <guillen.marce@gmail.com>
2025-10-16 23:15:49 -0300

    Expand Alembic version column length

--- migrations/versions/20251210_merge_public_survey_password_reset_heads.py
+++ migrations/versions/20251210_merge_public_survey_password_reset_heads.py
@@ -1,7 +1,7 @@
 """Merge public survey and password reset heads."""
 
 revision = "20251210_merge_public_survey_password_reset_heads"
-down_revision = ("1d4c9d7c5a8f", "20251201_password_reset_security")
+down_revision = ("1d4c9d7c5a8f", "20251209_expand_ver_len")
 branch_labels = None
 depends_on = None
 
